dd32d9951bf3761cfe26a6d37116ec02c4797b69
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-07-17 22:59:25 +0200

    Theming: Include base for additional themes, (3/5):
    Part (1/5):
    android_development
    
    Part (2/5):
    android_frameworks_base
    
    Part (3/5):
    android_frameworks_native
    
    Part (4/5):
    android_frameworks_support
    
    Part (5/5):
    android_packages_apps_DKSettings

--- include/android/configuration.h
+++ include/android/configuration.h
@@ -313,10 +313,18 @@ enum {
     ACONFIGURATION_UI_MODE_NIGHT_YES = 0x2,
 
     /**
-     * UI blackout mode: value that corresponds to
-     * <a href="@dacRoot/guide/topics/resources/providing-resources.html#NightQualifier">blackout</a> resource qualifier specified.
+     * UI night mode: value that corresponds to NightQualifier "darkkat"
      */
-    ACONFIGURATION_UI_MODE_NIGHT_BLACKOUT = 0x3,
+    ACONFIGURATION_UI_MODE_NIGHT_YES_DARKKAT = 0x3,
+
+    /**
+     * UI night mode: value that corresponds to NightQualifier "whiteout"
+     */
+    ACONFIGURATION_UI_MODE_NIGHT_NO_WHITEOUT = 0x4,
+    /**
+     * UI night mode: value that corresponds to NightQualifier "blackout"
+     */
+    ACONFIGURATION_UI_MODE_NIGHT_YES_BLACKOUT = 0x5,
 
     /** Screen width DPI: not specified. */
     ACONFIGURATION_SCREEN_WIDTH_DP_ANY = 0x0000,
